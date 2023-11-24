3d6f00ad0b90f131144a2aaa69fc7c79601b9f6f
CalinaThalya <calina.thalya@icloud.com>
2023-11-24 01:52:34 -0300

    Alterando nomenclaturas + Turma adicionada

--- gs-web/src/components/Rodape/Rodape.jsx
+++ gs-web/src/components/Rodape/Rodape.jsx
@@ -1,11 +1,14 @@
 export default function Rodape() {
     return (
       <footer className="w-full bg-gray-800 h-20 flex items-center justify-between">
-        <p className="ml-4 text-white font-semibold">Leonardo Lucena-RM552537</p>
-        <p className="ml-4 text-white font-semibold">Leonardo Lucena-RM552537</p>
-        <p className="ml-4 text-white font-semibold">Leonardo Lucena-RM552537</p>
-        <p className="ml-4 text-white font-semibold">Leonardo Lucena-RM552537</p>
-        <p className="ml-4 text-white font-semibold">Leonardo Lucena-RM552537</p>
+         <p className="ml-4 text-white font-bold text-lg">1TDSR </p>
+         <p className="ml-4 text-white font-semibold">Ana Paula - RM552513</p>
+         <p className="ml-4 text-white font-semibold">Calina Thalya - RM552523</p>
+         <p className="ml-4 text-white font-semibold">Geovana Ribeiro - RM99646</p>
+        <p className="ml-4 text-white font-semibold">Leonardo Lucena - RM552537</p>
+        <p className="ml-4 text-white font-semibold">Nathan Nunes - RM552539</p>
+        
+        
   
         <div className="flex items-center space-x-4 mr-4">
           <a href="https://github.com/LeonardoCamargo77/GS-Web" target="_blank" rel="noopener noreferrer" className="text-black">
