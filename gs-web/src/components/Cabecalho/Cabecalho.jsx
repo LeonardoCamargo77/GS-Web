0f00c29a8010d6884f2b4dd7cccb058937226799
CalinaThalya <calina.thalya@icloud.com>
2023-11-24 01:14:41 -0300

    ajustes no Cabecalho

--- gs-web/src/components/Cabecalho/Cabecalho.jsx
+++ gs-web/src/components/Cabecalho/Cabecalho.jsx
@@ -4,18 +4,18 @@ export default function Cabecalho() {
   return (
     <div>
       <header className="text-black p-2 flex items-center justify-between h-50 border-b border-orange-500">
-        <p className="flex items-center font-bold text-2xl hover:text-orange-500 border-r-2 orange pr-4 rounded">
+        <p className="flex items-center font-bold text-3xl hover:text-orange-500 border-r-2 orange pr-4 rounded">
           <Link href="/">PrevinaTech</Link>
           <img src="/img/coracaoo.png" alt="Logo" className="w-8 h-8 mr-2" />
         </p>
         <nav className="cabecalhoo flex items-center">
-          <p className="mx-0 text-lg font-semibold hover:bg-orange-600 rounded">
+          <p className="mx-0 text-xl font-semibold hover:bg-orange-600 rounded">
             <Link href="/previnatech/home">Home</Link>
           </p>
-          <p className="mx-0 text-lg font-semibold hover:bg-orange-600 rounded"> 
+          <p className="mx-0 text-xl font-semibold hover:bg-orange-600 rounded"> 
             <Link href="/previnatech/formulario">Formulário</Link>
           </p>
-          <p className="mx-0 text-lg font-semibold hover:bg-orange-600 rounded"> 
+          <p className="mx-0 text-xl font-semibold hover:bg-orange-600 rounded"> 
             <Link href="/previnatech/hospitais">Hospitais</Link>
           </p>
         </nav>
