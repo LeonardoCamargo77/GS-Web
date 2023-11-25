64c68687c2d9a0895b42dcb9fdd46df162b4048b
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2023-11-24 23:30:16 -0300

    Removendo rotação de imagem

--- gs-web/src/app/previnatech/(dinamicas)/formulario/page.jsx
+++ gs-web/src/app/previnatech/(dinamicas)/formulario/page.jsx
@@ -6,7 +6,7 @@ export default function Formulario() {
     <div className="mx-auto max-w-screen-xl">
       <h1 className="text-5xl font-serif text-center mt-16 mb-24">Médicos renomados das nossas unidades </h1>
       <div className="flex justify-center space-x-8">
-        <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
+        <div className="mb-2 overflow-hidden rounded-xl ">
           <figure className="flex flex-col items-right">
             <Image src="/img/medicos.webp" alt="Doença" width={800} height={800} />
             <Link href="http://localhost:8080/previnatech/webapi/medico">
