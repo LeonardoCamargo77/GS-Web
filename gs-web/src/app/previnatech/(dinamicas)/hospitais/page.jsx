129cbcb97cc9d33875454374848b05c188ba3bcf
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-11-24 21:07:45 -0300

    Inicio page unidades hospitalares

--- gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
+++ gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
@@ -6,6 +6,17 @@ export default function Hospitais() {
     <div className="mx-auto max-w-screen-xl">
       <h1 className="text-5xl font-serif text-center mt-16 mb-24">Hospitais</h1>
 
+      <div className="flex justify-center space-x-8">
+          <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
+          <figure className="flex flex-col items-center">
+            <Image src="/img/NotreDame.jpg" alt="NotreDame" width={500} height={500} />
+            <figcaption className="text-center font-semibold text-lg">Unidade - São Bernardo do Campo </figcaption>
+
+          </figure>
+          
+        </div>
+        </div>
+
      
     </div>
   );
