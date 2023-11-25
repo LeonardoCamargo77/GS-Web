27dca4f19c6ef16990b03fed5ab762076652a113
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-11-24 21:27:33 -0300

    Estilização page hospitais

--- gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
+++ gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
@@ -12,7 +12,7 @@ export default function Hospitais() {
           {/* Primeira Unidade */}
           <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
             <figure className="flex flex-col items-center">
-              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={500} height={500} />
+              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={900} height={900} />
               <figcaption className="text-center font-semibold text-lg">Unidade - São Bernardo do Campo </figcaption>
             </figure>
             <Link href="https://www.gndi.com.br/unidades/hospital-intermedica-abc">
@@ -22,11 +22,10 @@ export default function Hospitais() {
             </Link>
           </div>
 
-          {/* Segunda Unidade */}
           <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
             <figure className="flex flex-col items-center">
-              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={500} height={500} />
-              <figcaption className="text-center font-semibold text-lg">Unidade - São Bernardo do Campo </figcaption>
+              <Image src="/img/NotreDame2.jpg" alt="NotreDame" width={900} height={900} />
+              <figcaption className="text-center font-semibold text-lg">Unidade - São Paulo - Jardim do Mar </figcaption>
             </figure>
             <Link href="https://www.gndi.com.br/unidades/hospital-intermedica-abc">
               <div className="border border-orange-600 py-2 px-10 rounded-full text-black hover:bg-orange-600 hover:text-white cursor-pointer text-center">
@@ -36,12 +35,12 @@ export default function Hospitais() {
           </div>
         </div>
 
-        {/* Segunda Seção (Duas Fotos em Baixo) */}
+        
         <div className="flex justify-center space-x-8">
-          {/* Terceira Unidade */}
+         
           <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
             <figure className="flex flex-col items-center">
-              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={500} height={500} />
+              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={900} height={900} />
               <figcaption className="text-center font-semibold text-lg">Unidade - São Bernardo do Campo </figcaption>
             </figure>
             <Link href="https://www.gndi.com.br/unidades/hospital-intermedica-abc">
@@ -51,10 +50,10 @@ export default function Hospitais() {
             </Link>
           </div>
 
-          {/* Quarta Unidade */}
+          
           <div className="mb-2 overflow-hidden rounded-xl transform hover:rotate-3 transition-transform duration-300">
             <figure className="flex flex-col items-center">
-              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={500} height={500} />
+              <Image src="/img/NotreDame.jpg" alt="NotreDame" width={900} height={900} />
               <figcaption className="text-center font-semibold text-lg">Unidade - São Bernardo do Campo </figcaption>
             </figure>
             <Link href="https://www.gndi.com.br/unidades/hospital-intermedica-abc">
