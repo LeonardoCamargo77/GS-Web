fd4f2a64b58bbcf5a847d543ff5d7e78f4f3c29c
anapaulans <rm552513@fiap.com.br>
2023-11-24 22:36:39 -0300

    Ajustes de url da api

--- gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
+++ gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
@@ -58,16 +58,15 @@ export default function Hospitais() {
       </div>
 
       <div className="mb-2 overflow-hidden rounded-xl">
-        <figure className="flex flex-col items-center">
-          <Image src="/img/NotreDame4.jpg" alt="NotreDame" width={800} height={800} />
-          <figcaption className="text-center font-semibold text-lg">Unidade - Centro Clínico Jundiaí </figcaption>
-        </figure>
-        <Link href="https://www.gndi.com.br/unidades/centro-clinico-jundiai">
+      <h1 className="text-3xl font-semibold text-center mt-16 mb-24">Confira os médicos renomados das unidades hospitalares ao clicar no botão!</h1>
+        <Link href="http://localhost:8080/previnatech/webapi/medico">
           <div className="border border-orange-600 py-2 px-10 rounded-full text-black hover:bg-orange-600 hover:text-white cursor-pointer text-center">
-            Ver Detalhes
+            Clique Aqui
           </div>
         </Link>
       </div>
+
+      
 
     
       <div className="mb-8"></div>
