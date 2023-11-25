c1c2cac6f1bfeca1ba98be9c30ef01b4e6df7552
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-11-24 22:10:05 -0300

    Acrescentando API

--- gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
+++ gs-web/src/app/previnatech/(dinamicas)/hospitais/page.jsx
@@ -57,6 +57,18 @@ export default function Hospitais() {
         </Link>
       </div>
 
+      <div className="mb-2 overflow-hidden rounded-xl">
+        <figure className="flex flex-col items-center">
+          <Image src="/img/NotreDame4.jpg" alt="NotreDame" width={800} height={800} />
+          <figcaption className="text-center font-semibold text-lg">Unidade - Centro Clínico Jundiaí </figcaption>
+        </figure>
+        <Link href="https://www.gndi.com.br/unidades/centro-clinico-jundiai">
+          <div className="border border-orange-600 py-2 px-10 rounded-full text-black hover:bg-orange-600 hover:text-white cursor-pointer text-center">
+            Ver Detalhes
+          </div>
+        </Link>
+      </div>
+
     
       <div className="mb-8"></div>
     </div>
